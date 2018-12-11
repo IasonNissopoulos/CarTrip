767c745404c364ca057dd14cad625f932702374d
IasonNissopoulos <t8150097@dias.aueb.gr>
2018-12-11 23:48:13 +0200

    Added test models

--- server/cartrip/admin.py
+++ server/cartrip/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Engine, Location, Car, Exhibition, Excursion, Bundle, Post, Comment
+
+admin.site.register(Engine)
+admin.site.register(Location)
+admin.site.register(Car)
+admin.site.register(Exhibition)
+admin.site.register(Excursion)
+admin.site.register(Bundle)
+#admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
--- server/cartrip/models.py
+++ server/cartrip/models.py
@@ -9,7 +9,9 @@ class Engine(models.Model) :
     title = models.CharField(max_length=200)
     engine_cubic_centimeters = models.CharField(max_length=20)
     engine_manufacturer = models.CharField(max_length=20)
-
+    def __str__(self):
+        return self.title
+#Location in progress
 class Location(models.Model) :
     location_address = models.CharField(max_length=200)
     map_location = models.CharField(max_length=200)
@@ -19,8 +21,10 @@ class Car(models.Model) :
     model = models.CharField(max_length=200)
     year = models.IntegerField('year', default=0)
     color = models.CharField(max_length=200)
-    engine = models.ForeignKey(Engine, on_delete=models.CASCADE) #in case of a car with two engines this will need to change
-    extra_information = models.CharField(max_length=500)
+    engine = models.ForeignKey(Engine, on_delete=models.CASCADE, blank=True) #in case of a car with two engines this will need to change
+    extra_information = models.CharField(max_length=500, null=True, blank=True)
+    def __str__(self):
+        return "%s %s %s" % (self.company, self.model, self.year)
     #maybe i can manage photos with photologue django
     #car_image =
 class Exhibition(models.Model) :
@@ -30,39 +34,35 @@ class Exhibition(models.Model) :
     cars = models.ManyToManyField(Car)
     description = models.TextField(max_length=800, blank=True, null=True)
     price = models.IntegerField('price', default=0)
-
+    def __str__(self):
+        return self.title
 class Excursion(models.Model) :
     title = models.CharField(max_length=200, unique=True)
     location = models.ManyToManyField(Location)
     date = models.DateTimeField('excursionDate')
     price = models.IntegerField('price', default=0)
     cars = models.ManyToManyField(Car)
-
+    def __str__(self):
+        return self.title
 class Bundle(models.Model) :
     title = models.CharField(max_length=200, unique=True)
     exhibitions = models.ManyToManyField(Exhibition)
     excursion = models.ManyToManyField(Excursion)
     date = models.DateTimeField('BundleDates')
     price = models.IntegerField('price', default=0)
-class User(models.Model) :
-    type = models.IntegerField('userType', default=0)
-    username = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    surname = models.CharField(max_length=200)
-    exhibitions = models.ForeignKey(Exhibition, on_delete=models.CASCADE)
-    bundles = models.ForeignKey(Bundle, on_delete=models.CASCADE)
-    excursions = models.ForeignKey(Excursion, on_delete=models.CASCADE)
-    password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
 #class Like(models.Model) :
     #recipient = models.ForeignKey(User, on_delete=models.CASCADE)
     #giver = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Post(models.Model) :
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(default='Post')
     #likes = models.ManyToManyField(Like)
-    post_date = models.DateTimeField(default=datetime.datetime.now() )
+    post_date = models.DateTimeField(auto_now=True )
 class Comment(models.Model) :
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment_date = models.DateTimeField(default=datetime.datetime.now())
+    comment_text = models.TextField(default='Comment')
+    comment_date = models.DateTimeField(auto_now=True)
