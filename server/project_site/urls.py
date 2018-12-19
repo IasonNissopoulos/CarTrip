b3225f9ff8e1e16c5ea423098f0f45fe5d47c40b
IasonNissopoulos <t8150097@dias.aueb.gr>
2018-12-19 17:37:24 +0200

    Change db and paths

--- server/project_site/urls.py
+++ server/project_site/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('cartrip/', include('cartrip.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
